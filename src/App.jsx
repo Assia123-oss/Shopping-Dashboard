f22cd5117a40061b9bcb6ec5d191c49775a9a1f7
Teta Assia <tetaassia54@gmail.com>
2024-04-17 17:38:59 -0700

    Just finished the whole application

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import './App.css'
-import Header from './Header'
-import Sidebar from './Sidebar'
-import Home from './Home'
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+import { useState } from "react";
 
 function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
 
   return (
- <div className='grid-container'>
-    <Header/>
-    <Sidebar/>
-    <Home/>
- </div>
-  )
+    <div className="grid-container">
+      <Header OpenSidebar={OpenSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -9,14 +9,14 @@ import {
   BsMenuButtonWideFill,
   BsFillGearFill} from "react-icons/bs";
 
-function Sidebar() {
+function Sidebar({openSidebarToggle, OpenSidebar}) {
   return (
-    <aside id="sidebar">
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive" : ""} >
       <div className="sidebar-title">
         <div className="sidebar-brand">
           <BsCart3 className="icon_header" /> SHOP
         </div>
-        <span className="icon close_icon">X</span>
+        <span className="icon close_icon" onClick={OpenSidebar}>X</span>
       </div>
 
       <ul className="sidebar-list">
